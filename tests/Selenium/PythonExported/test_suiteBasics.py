e40f0e8291678608248be8cf87b1829d2b290bce
Marcelo Linhares Castro <marlinhares@gmail.com>
2025-11-07 14:45:23 -0300

    Ajustes Trailing Spaces

--- tests/Selenium/PythonExported/test_suiteBasics.py
+++ tests/Selenium/PythonExported/test_suiteBasics.py
@@ -14,20 +14,20 @@ class TestSuiteBasics():
   def setup_method(self, method):
     desired_capabilities = DesiredCapabilities.CHROME.copy()
     desired_capabilities['acceptInsecureCerts'] = True
-    
+
     self.driver = webdriver.Remote(command_executor='http://seleniumchrome:4444/wd/hub', desired_capabilities=desired_capabilities)
     self.vars = {}
-  
+
   def teardown_method(self, method):
     self.driver.quit()
-  
+
   def wait_for_window(self, timeout = 2):
     time.sleep(round(timeout / 1000))
     wh_now = self.driver.window_handles
     wh_then = self.vars["window_handles"]
     if len(wh_now) > len(wh_then):
       return set(wh_now).difference(set(wh_then)).pop()
-  
+
   def test_loginBasico(self):
     self.driver.get("<<PROTOCOLO>>://<<HOST>>/sip/login.php?sigla_orgao_sistema=ORGAO1&sigla_sistema=SEI&infra_url=L3NlaS8=")
     WebDriverWait(self.driver, 30).until(expected_conditions.element_to_be_clickable((By.ID, "txtUsuario")))
@@ -46,10 +46,10 @@ class TestSuiteBasics():
     elements = self.driver.find_elements(By.XPATH, "//td[contains(.,\'Módulo de Estatisticas do SEI\')]")
     assert len(elements) > 0
     self.driver.find_element(By.XPATH, "//span[contains(.,\'Agendamentos\')]").click()
-    
+
     #todo
     # reativar as 3 linhas abaixo qundo o modulo estatisticas funcionar para o postgres
-    
+
     #WebDriverWait(self.driver, 30).until(expected_conditions.presence_of_element_located((By.XPATH, "//td[contains(.,\'MdEstatisticasAgendamentoRN :: coletarIndicadores\')]")))
     #elements = self.driver.find_elements(By.XPATH, "//td[contains(.,\'MdEstatisticasAgendamentoRN :: coletarIndicadores\')]")
     #assert len(elements) > 0
@@ -64,20 +64,20 @@ class TestSuiteBasics():
     self.driver.find_element(By.CSS_SELECTOR, "#divInfraBarraComandosInferior > #btnSalvar").click()
     WebDriverWait(self.driver, 30).until(expected_conditions.presence_of_element_located((By.XPATH, "//iframe[@id=\'ifrArvore\']")))
     WebDriverWait(self.driver, 30).until(expected_conditions.presence_of_element_located((By.XPATH, "//iframe[@id=\'ifrVisualizacao\'] | //iframe[@id=\'ifrConteudoVisualizacao\']")))
-    
+
     elements = self.driver.find_elements(By.XPATH, "//iframe[@id=\'ifrConteudoVisualizacao\']")
     self.bolSei41 = len(elements) > 0
-    
+
     self.driver.switch_to.frame(1)
     WebDriverWait(self.driver, 30).until(expected_conditions.visibility_of_element_located((By.XPATH, "//img[@alt=\'Incluir Documento\']")))
     self.driver.find_element(By.XPATH, "//img[@alt=\'Incluir Documento\']").click()
-    
+
     if(self.bolSei41):
         time.sleep(5)
         self.driver.switch_to.default_content()
         self.driver.switch_to.frame(self.driver.find_element(By.XPATH, "//iframe[@id='ifrConteudoVisualizacao']"))
         self.driver.switch_to.frame(self.driver.find_element(By.XPATH, "//iframe[@id='ifrVisualizacao']"))
-    
+
     self.driver.find_element(By.XPATH, "//a[text()='Despacho']").click()
     self.driver.find_element(By.CSS_SELECTOR, "#divOptPublico .infraRadioLabel").click()
     self.vars["window_handles"] = self.driver.window_handles
@@ -95,13 +95,13 @@ class TestSuiteBasics():
     self.driver.switch_to.frame(1)
     WebDriverWait(self.driver, 30).until(expected_conditions.visibility_of_element_located((By.XPATH, "//img[@alt=\'Incluir Documento\']")))
     self.driver.find_element(By.XPATH, "//img[@alt=\'Incluir Documento\']").click()
-    
+
     if(self.bolSei41):
-        time.sleep(5)    
+        time.sleep(5)
         self.driver.switch_to.default_content()
         self.driver.switch_to.frame(self.driver.find_element(By.XPATH, "//iframe[@id='ifrConteudoVisualizacao']"))
         self.driver.switch_to.frame(self.driver.find_element(By.XPATH, "//iframe[@id='ifrVisualizacao']"))
-    
+
     WebDriverWait(self.driver, 30).until(expected_conditions.visibility_of_element_located((By.LINK_TEXT, "Externo")))
     self.driver.find_element(By.LINK_TEXT, "Externo").click()
     WebDriverWait(self.driver, 30).until(expected_conditions.element_to_be_clickable((By.ID, "selSerie")))
@@ -118,15 +118,15 @@ class TestSuiteBasics():
     WebDriverWait(self.driver, 30).until(expected_conditions.presence_of_element_located((By.XPATH, "//iframe[@id=\'ifrfrmAnexos\']")))
     WebDriverWait(self.driver, 30).until(expected_conditions.presence_of_element_located((By.XPATH, "//form[@id=\'frmAnexos\']/div[2]/table/tbody/tr[1]/td[2]")))
     self.driver.find_element(By.CSS_SELECTOR, "#divInfraBarraComandosInferior > #btnSalvar > .infraTeclaAtalho").click()
-    
+
     self.driver.switch_to.default_content()
     WebDriverWait(self.driver, 30).until(expected_conditions.presence_of_element_located((By.XPATH, "//iframe[@id=\'ifrArvore\']")))
     WebDriverWait(self.driver, 30).until(expected_conditions.presence_of_element_located((By.XPATH, "//iframe[@id=\'ifrVisualizacao\'] | //iframe[@id=\'ifrConteudoVisualizacao\']")))
     self.driver.find_element(By.XPATH, "//div[@id='divInfraBarraSistemaPadrao']/div/div/button | //div[@id='divInfraBarraSistemaPadrao']/div/div/a[@id='lnkInfraMenuSistema']").click()
     WebDriverWait(self.driver, 30).until(expected_conditions.visibility_of_element_located((By.XPATH, "//ul[@id='infraMenu']//span[text()='Controle de Processos']")))
     self.driver.find_element(By.XPATH, "//ul[@id='infraMenu']//span[text()='Controle de Processos']").click()
-    
-    
+
+
     WebDriverWait(self.driver, 30).until(expected_conditions.presence_of_element_located((By.XPATH, "//a[contains(.,\'99990\')]")))
     elements = self.driver.find_elements(By.XPATH, "//a[contains(.,\'99990\')]")
     assert len(elements) > 0
@@ -134,4 +134,4 @@ class TestSuiteBasics():
     WebDriverWait(self.driver, 30).until(expected_conditions.visibility_of_element_located((By.ID, "txtUsuario")))
     elements = self.driver.find_elements(By.ID, "txtUsuario")
     assert len(elements) > 0
-  
+
